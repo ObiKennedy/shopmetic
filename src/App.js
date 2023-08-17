4b405322e17a6daca0d1354cc3a6c1c589a02309
Obi kennedy <ahmdexter2000@gmail.com>
2023-08-17 20:40:58 +0100

    shopmetic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import React from 'react';
+import Home from './Pages/Home';
+import Header from './Components/Header';
 
-function App() {
-  return (
+function App(){
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <Routes>
+          <Route path='*' element={<MainApp/>}/>
+          </Routes>
+        </Router>
     </div>
   );
 }
 
 export default App;
+
+const MainApp = () =>{
+  return(
+    <div>
+            <Header/>
+            <Routes>
+                <Route path='*' element={<Home/>}/>
+            </Routes>
+    </div>
+  )
+}
